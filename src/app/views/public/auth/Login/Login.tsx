126cd2619b87bfed60235ece9bd8669b089cd574
Angir777 <90105508+Angir777@users.noreply.github.com>
2024-08-23 22:36:52 +0200

    Test format

--- src/app/views/public/auth/Login/Login.tsx
+++ src/app/views/public/auth/Login/Login.tsx
@@ -2,10 +2,6 @@ import { FC } from 'react';
 
 interface LoginProps {}
 
-const Login: FC<LoginProps> = () => (
-  <div>
-    Login Component
-  </div>
-);
+const Login: FC<LoginProps> = () => <div>Login Component</div>;
 
 export default Login;
